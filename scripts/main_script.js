1be6ef74b692d2e2c0746a13a50b9636331a1058
HeightenCM <hasconstantin@proton.me>
2025-01-06 15:43:09 +0200

    Trying to figure out positioning this mess

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,5 +1,13 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let canvasAnim = document.getElementById('canvasAnimation');
+animContext = canvasAnim.getContext('2d');
+let bus1 = new Image();
+bus1.src='assets/images/bus1.gif';
+bus1.onload = function() {
+    animContext.drawImage(bus1, 50, 50, 124, 33);
+};
