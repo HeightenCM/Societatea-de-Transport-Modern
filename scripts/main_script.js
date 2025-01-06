551c6816c4e3092b5faec62d0a26724e315bd752
HeightenCM <hasconstantin@proton.me>
2025-01-06 20:48:10 +0200

    Added the transition to the map

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -22,7 +22,7 @@ changetheme.addEventListener('click', () => {
 
 //list of destinations, might add more idk
 let destinationList = [
-    {"name":"ASE","longitude":"44.447437","latitude":"26.097938"},
+    {"name":"ASE","latitude":"44.447437","longitude":"26.097938"},
     {"name":"Unibuc","longitude":"44.447437","latitude":"26.097938"},
     {"name":"Piata Victoriei","longitude":"44.447437","latitude":"26.097938"},
     {"name":"Obor","longitude":"44.447437","latitude":"26.097938"},
@@ -49,5 +49,27 @@ destinationForm.addEventListener('submit', (event)=>{
     const selectedDest = document.getElementById('destinationSelector').value;
     const destination = destinationList.find(dest => dest.name===selectedDest);
 
+    const containerDest = document.getElementById('containerthingie');
 
+    if(destination){ //Might not need this check but just to be sure
+        containerDest.remove();
+        initializeMap(destination.name,destination.latitude, destination.longitude);
+    } else{
+        alert("Didn't select a destination!");
+    }
 });
+
+//Initializing the map based on given coordinates
+function initializeMap(destinationName, latitude, longitude) {
+    const mapDiv = document.getElementById("map");
+    mapDiv.style.display = "block"; // And magically the map appears
+
+    const map = L.map('map').setView([latitude, longitude], 13); // Centers map on destination (for now)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(destinationName)
+        .openPopup();
+}
